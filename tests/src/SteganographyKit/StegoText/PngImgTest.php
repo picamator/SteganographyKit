4b0b8777a6a1d95e850c51cb1d48aad7a1135600
Sergii Pryz <picamator@gmail.com>
2014-06-15 00:28:04 +0200

    Updated unitTest for PngImg

--- tests/src/SteganographyKit/StegoText/PngImgTest.php
+++ tests/src/SteganographyKit/StegoText/PngImgTest.php
@@ -19,10 +19,17 @@ class PngImgTest extends BaseTest
     public function testGetBinaryData(array $options) 
     {
         $options['path'] = $this->dataPath . '/' . $options['path'];
+        $xIndex = 10;
+        $yIndex = 10;
         
-        $pngImg     = new PngImg($options); 
-        $result     = $pngImg->getBinaryData(10, 10);
-        var_dump($result);
+        $pngImg = new PngImg($options); 
+        $result = $pngImg->getBinaryData($xIndex, $yIndex);
+        foreach($result[0] as $item) {
+            $this->assertFalse(empty($item['red']));
+            $this->assertFalse(empty($item['green']));
+            $this->assertFalse(empty($item['blue']));
+        }      
+        $this->assertEquals($xIndex, count($result[0]));
     }
     
     public function providerGetBinaryData() 
